cbb63124a6eba4ed8633717a243a8912592a5fa4
Polina <polina.solodkova@gmail.com>
2025-03-09 14:08:43 +0300

    fix git work

---
+++ 2048/Assets/Scripts/TileBoard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBoard : MonoBehaviour
+{
+public Tile tilePrefab;
+
+   private TileGrid grid;
+    private List<Tile> tiles;
+
+
+}
---
+++ 2048/Assets/Scripts/TileGrid.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+public class TileGrid : MonoBehaviour
+{
+    public TileRow[] rows { get; private set; }
+    public TileCell[] cells { get; private set; }
+
+ public int Size => cells.Length;
+    public int Height => rows.Length;
+    public int Width => Size / Height;
+
+     private void Awake()
+    {
+        rows = GetComponentsInChildren<TileRow>();
+        cells = GetComponentsInChildren<TileCell>();
+
+        // for (int i = 0; i < cells.Length; i++) {
+        //     cells[i].coordinates = new Vector2Int(i % Width, i / Width);
+        // }
+    }
+
+    private void Start(){
+        for(int y =0; y < rows.Length; y++){
+            for(int x =0; x < rows[y].cells.Length; x++){
+                rows[y].cells[x].coordinates = new Vector2Int(x, y);
+            }
+        }
+    }
+}
